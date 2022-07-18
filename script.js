10ebfae1f72b697a20f2cb330820e49887c769c3
Gary Hoang <garyhoangvinh@gmail.com>
2022-07-18 14:22:03 -0700

    made dark mode but ton work

---
+++ script.js
@@ -0,0 +1,9 @@
+let setDark = function() {
+    let main = document.querySelector("main");
+    main.style.backgroundColor = "black";
+    main.style.color = "white";
+}
+
+
+let dmButton = document.querySelector("button#darkMode");
+dmButton.addEventListener("click", setDark);
