b0908996162f49591950f0b58cc8819cd85223c5
Gary Hoang <garyhoangvinh@gmail.com>
2022-07-18 14:52:38 -0700

    4

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+let dmButton = document.querySelector("button#darkMode");
+
 let setDark = function() {
     let main = document.querySelector("main");
-    main.style.backgroundColor = "black";
-    main.style.color = "white";
+    let body = document.querySelector("body");
+    if (main.style.backgroundColor === "black") {
+        main.style.backgroundColor = "white";
+        main.style.color = "black";
+        body.style.backgroundColor = "white";
+        dmButton.style.backgroundColor = "black";
+        dmButton.style.color = "white";
+        dmButton.textContent = "Dark Mode";
+    } else {
+        main.style.backgroundColor = "black";
+        main.style.color = "white";
+        body.style.backgroundColor = "black";
+        dmButton.style.backgroundColor = "white";
+        dmButton.style.color = "black";
+        dmButton.textContent = "Light Mode";
+    }
 }
 
-
-let dmButton = document.querySelector("button#darkMode");
 dmButton.addEventListener("click", setDark);
