5a11519d7fc237973ff590aa45ae8f47bb7d2f46
Gary Hoang <garyhoangvinh@gmail.com>
2022-07-18 16:02:42 -0700

    5

---
+++ script2.js
@@ -0,0 +1,17 @@
+let foods = ["pizza", "corn", "hot dogs", "ramen"];
+
+// for of loop
+// for (let food of foods) {
+//     console.log("I like to eat", food);
+// }
+
+// let printFood = function(food) {
+//     console.log("I like to eat", food);
+// }
+
+// foods.forEach(printFood);
+
+foods.forEach(function(food) {
+    console.log("I like to eat", food);
+});
+
